ae10e0fa89d1b94b0a2052404b13c05942f24224
Alessandro <ale-remo@hotmail.com>
2017-09-26 00:06:43 -0300

    Adding margins to albums cards

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,29 +1,33 @@
 import React, {Component} from 'react';
-import {Image, Linking} from 'react-native';
+import {Image, Linking, View} from 'react-native';
 import { Button, Card, CardItem, Left, Body, Text, Thumbnail} from 'native-base';
 
 class AlbumCard extends Component {
     render() {
         return (
-            <Card>
-                <CardItem>
-                    <Left>
-                        <Thumbnail source={{uri: this.props.data.artistPicture}} />
-                        <Body>
-                        <Text>{this.props.data.albumName}</Text>
-                        <Text note>{this.props.data.artistName}</Text>
-                        </Body>
-                    </Left>
-                </CardItem>
+            <View style={{ marginTop: 5, marginBottom: 5 }}>
+                <Card>
+                    <CardItem>
+                        <Left>
+                            <Thumbnail source={{uri: this.props.data.artistPicture}} />
+                            <Body>
+                            <Text>{this.props.data.albumName}</Text>
+                            <Text note>{this.props.data.artistName}</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
 
-                <CardItem cardBody>
-                    <Image source={{uri: this.props.data.albumPicture}} style={{height: 350, width: null, flex: 1}}/>
-                </CardItem>
+                    <CardItem cardBody>
+                        <Image source={{uri: this.props.data.albumPicture}} style={{height: 350, width: null, flex: 1}}/>
+                    </CardItem>
 
-                <Button full info onPress={() => Linking.openURL(this.props.data.albumLink).catch(err => Alert.alert(err)) }>
-                    <Text>Buy Now</Text>
-                </Button>
-            </Card>
+                    <Button full info onPress={() => Linking.openURL(this.props.data.albumLink).catch(err => Alert.alert(err)) }>
+                        <Text>Buy Now</Text>
+                    </Button>
+                </Card>
+            </View>
+
+
         )
     }
 }
